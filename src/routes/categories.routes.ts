1aa1a56d6f5d01a1924fa8a722823c1244166225
kaynansc <kaynansc@gmail.com>
2021-04-28 19:58:50 -0300

    Criando repositório de categoria

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,24 +1,14 @@
 import { Router } from "express";
-import { v4 as uuidV4 } from "uuid";
 
-import { Category } from "../model/Category";
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
 
 const categoriesRoutes = Router();
-
-const categories: Category[] = [];
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  const category = new Category();
-
-  Object.assign(category, {
-    name,
-    description,
-    created_at: new Date(),
-  });
-
-  categories.push(category);
+  categoriesRepository.create({ name, description });
 
   return response.status(201).send();
 });
