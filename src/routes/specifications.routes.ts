766ce51a94d6020a2d6f3a0eb668ba1906b0639c
kaynansc <kaynansc@gmail.com>
2021-05-18 21:38:33 -0300

    Autenticação nas rotas

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 
+import { ensureAuthtenticated } from "../middlewares/ensureAuthtenticated";
 import { CreateSpecificationController } from "../modules/cars/useCases/createSpecification/CreateSpecificationController";
 
 const specificationsRoutes = Router();
 
 const createSpecificationController = new CreateSpecificationController();
 
+specificationsRoutes.use(ensureAuthtenticated);
 specificationsRoutes.post("/", createSpecificationController.handle);
 
 export { specificationsRoutes };
