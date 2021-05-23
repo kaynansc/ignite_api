3d6aad3d1b2919c73fd1ec18ef840d1cb6dddf04
kaynansc <kaynansc@gmail.com>
2021-05-23 17:24:25 -0300

    TDD na prática

---
+++ src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
@@ -0,0 +1,25 @@
+import { CarsRepositoryInMemory } from "@modules/cars/repositories/in-memory/CarsRepositoryInMemory";
+
+import { CreateCarUseCase } from "./CreateCarUseCase";
+
+let createCarUseCase: CreateCarUseCase;
+let carsRepositoryInMemory: CarsRepositoryInMemory;
+
+describe("Create Car", () => {
+  beforeEach(() => {
+    carsRepositoryInMemory = new CarsRepositoryInMemory();
+    createCarUseCase = new CreateCarUseCase(carsRepositoryInMemory);
+  });
+
+  it("should be able to create a new car", async () => {
+    await createCarUseCase.execute({
+      brand: "Brand Car",
+      category_id: "12321321",
+      daily_rate: 100,
+      description: "Description Car",
+      fine_amount: 60,
+      license_plate: "ABC-1234",
+      name: "Name Car",
+    });
+  });
+});
