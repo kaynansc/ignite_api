87870432996fa63fe600964375c33d7dd7a5cb65
kaynansc <kaynansc@gmail.com>
2021-05-31 20:29:22 -0300

    Criando migrations do aluguel

--- src/shared/infra/typeorm/migrations/1621290953619-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1621290953619-CreateUsers.ts
@@ -9,6 +9,7 @@ export class CreateUsers1621290953619 implements MigrationInterface {
           {
             name: "id",
             type: "uuid",
+            isPrimary: true,
           },
           {
             name: "name",
