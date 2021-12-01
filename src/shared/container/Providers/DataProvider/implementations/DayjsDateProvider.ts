f40798f77648a3f6f53a636303a17b46b327d2d3
Kaynan Sampaio <kaynansc@gmail.com>
2021-11-30 21:37:13 -0300

    Criando caso de uso para recuperar senha

--- src/shared/container/Providers/DataProvider/implementations/DayjsDateProvider.ts
+++ src/shared/container/Providers/DataProvider/implementations/DayjsDateProvider.ts
@@ -6,6 +6,7 @@ import { IDateProvider } from "../IDateProvider";
 dayjs.extend(utc);
 
 class DayjsDateProvider implements IDateProvider {
+
   getDateNow(): Date {
     return dayjs().toDate();
   };
@@ -30,6 +31,10 @@ class DayjsDateProvider implements IDateProvider {
 
   addDays(days: number): Date {
     return dayjs().add(days, "days").toDate();
+  }
+
+  addHours(hours: number): Date {
+    return dayjs().add(hours, "hours").toDate();
   }
 }
 
