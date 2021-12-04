80f2ae84f22d7d0cdf5748639711232e8e27f164
Kaynan Sampaio <kaynansc@gmail.com>
2021-12-04 13:17:06 -0300

    Provider de Upload

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -10,7 +10,7 @@ import uploadConfig from "../../../../config/upload";
 import { ensureAdmin } from "../middlewares/ensureAdmin";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
-const upload = multer(uploadConfig.upload("./tmp/cars"));
+const upload = multer(uploadConfig);
 
 const carsRoutes = Router();
 
