344fdfdc9bf0f34e3cb714b9c38946b0b58036d5
Kaynan Sampaio <kaynansc@gmail.com>
2021-12-11 11:59:15 -0300

    Configurando Sentry

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -4,6 +4,8 @@ import express, { Request, Response, NextFunction } from "express";
 import "dotenv/config";
 import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
 
 import { AppError } from "@shared/errors/AppError";
 import createConnection from "@shared/infra/typeorm";
@@ -11,6 +13,7 @@ import createConnection from "@shared/infra/typeorm";
 import swaggerFile from "../../../swagger.json";
 import { router } from "./routes";
 import rateLimiter from "@shared/infra/http/middlewares/rateLimiter";
+
 
 import "@shared/container";
 import upload from "@config/upload";
@@ -20,6 +23,20 @@ const app = express();
 
 app.use(rateLimiter);
 
+if (process.env.SENTRY_DSN.length > 50) {
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    integrations: [
+      new Sentry.Integrations.Http({ tracing: true }),
+      new Tracing.Integrations.Express({ app }),
+    ],
+    tracesSampleRate: 1.0,
+  });
+
+  app.use(Sentry.Handlers.requestHandler());
+  app.use(Sentry.Handlers.tracingHandler());
+}
+
 app.use(express.json());
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
@@ -27,21 +44,22 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
 app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
-
-
 app.use(router);
 
-app.use(
-  (err: Error, request: Request, response: Response, next: NextFunction) => {
-    if (err instanceof AppError) {
-      return response.status(err.statusCode).json({ message: err.message });
-    }
+if (process.env.SENTRY_DSN.length > 50) {
+  console.log("Sentry is running!");
+  app.use(Sentry.Handlers.errorHandler());
+}
 
-    return response.status(500).json({
-      status: "error",
-      message: `Internal server error - ${err.message}`,
-    });
-  }
+app.use(
+  Sentry.Handlers.errorHandler({
+    shouldHandleError(error) {
+      if (error.status === 429 || error.status === 500) {
+        return true;
+      }
+      return false;
+    },
+  })
 );
 
 export { app };
